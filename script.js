487bf980126da6f30d40f4d2ff82367e1739ab09
raut-andrey <andrey.raut@fusion-team.com>
2019-05-23 15:40:33 +0300

    save to do to LC

--- script.js
+++ script.js
@@ -1,68 +1,121 @@
-/*  adding a new task after button click  */
-$(document).ready(function () {
+const TODOS_KEY = 'todos';
 
-    $('#addtoDo').click(function () {
-        let taskText = $('#toDoName').val();
-        const currentdate = new Date($.now());
-        const datetime = currentdate.getDate() + "/" +
-            (currentdate.getMonth() + 1) + "/" +
-            currentdate.getFullYear() + "  " +
-            currentdate.getHours() + ":" +
-            currentdate.getMinutes();
+let todos = [];
+document.getElementById("todo-list").innerHTML = todos;
 
-        $('#todo-list').prepend(
-            `<li>
-                <input type = "checkbox">
+
+$(document).ready(function() {
+    initTodos();
+    createTodo(taskText);
+});
+
+/* create new to do after clicking the button "Add" */
+
+$('#addtoDo').click(function() {
+    let taskText = $('#toDoName').val();
+
+    /**
+     * 1) Create todo object 
+     * 2) Put this obj to todos 
+     * 3) Save todos to LS 
+     * 4) Draw todo 
+     */
+
+    const todo = createTodo(taskText);
+    todos.push(todo);
+    saveTodosToStorage(todos);
+    drawTodo(todo);
+
+    /* add date and time near the task */
+
+    const currentdate = new Date($.now());
+    const datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + "  " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes();
+
+});
+
+/*  add click on enter button  */
+$('#toDoName').keyup(function(event) {
+    if (event.keyCode == 13) {
+        $('#addtoDo').click();
+    };
+});
+
+
+function drawTodo(todo) {
+    //todo.taskText, todo.datetime 
+    $('#todo-list').append(
+        `<li>
+                <input type = "checkbox" id="stat">
                 ${taskText} ${datetime}
-            </li>`);
-        const newTask = {
-            id: +new Date(),
-            taskText: taskText,
-            date: new Date(),
-            isActive: false
-        }
-        setDataToLC(newTask)
+        </li>`
+    )
+}
+
+function drawTodos(todos) {
+    todos.forEach(todo => {
+        return drawTodo(todo);
     });
-    /*  add click on enter button  */
-    $('#toDoName').keyup(function (event) {
-        if (event.keyCode == 13) {
-            $('#addtoDo').click();
-        }
-    });
-})
 
-function drawTaskListFromLC() {
-    const taskItem = localStorage.getItem('array')
-    drawTaskListFromLC();
 }
 
-const todos = [{
-    'key': 'val'
-    
-}];
-let str_arr = JSON.stringify(todos);
-console.log('------------------------------------');
-console.log(str_arr);
-console.log('------------------------------------');
-localStorage.setItem('array', str_arr)
-const obj = {
-    key: 'val1',
-    name: '',
-    date: '',
-    isActive: false
-}
-//localStorage.setItem('array', retstr_arr)
-let retstr_arr = JSON.parse(localStorage.getItem('array'));
-retstr_arr.push(obj)
 
-let otherCheckbox = 
+/**
+ * 1) Get todos from LS
+ * 2) todos.forEach(todo => drawTodo(todo))
+ */
 
-function getDataFromLC() {
-    return JSON.parse(localStorage.getItem('array') || '[]')
+function initTodos() {
+
+    todos = getTodosFromStorage();
+    drawTodos(todos);
 }
 
-function setDataToLC(data) {
-    const currentTaskList = getDataFromLC();
-    currentTaskList.push(data)
-    localStorage.setItem('array', JSON.stringify(currentTaskList))
+
+function drawTaskListFromLS() {
+    const taskItem = localStorage.getItem('array');
 }
+
+
+function createTodo(taskText) {
+    return {
+        id: +new Date(),
+        taskText: taskText,
+        date: new Date(),
+        isActive: false,
+
+    }
+}
+
+
+function saveTodosToStorage(todos) {
+    const serializedTodos = JSON.stringify(todos);
+    localStorage.setItem(TODOS_KEY, serializedTodos);
+}
+
+function getTodosFromStorage() {
+    const todos = JSON.parse(localStorage.getItem(TODOS_KEY));
+    if (!todos) { return []; }
+    return todos;
+}
+
+
+/**
+ * написала таск
+ * добавила
+ * создала объект туду
+ * добавила в массив
+ * записала в ЛС
+ * сохранила в ЛС
+ * перезагрузила страницу
+ * таски на месте - отрисовка из ЛС
+ */
+
+/** для пустой страницы
+ * открыла страницу
+ * вижу только поле для ввода таска(??)
+ * потому что ЛС выводит пустой массив
+ */
